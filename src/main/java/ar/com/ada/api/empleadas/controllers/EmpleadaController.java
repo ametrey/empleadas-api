91bfbfb3cbcdea92cecf72c485f0e6e14980d276
ametrey <aetcheverryrey@gmail.com>
2021-07-14 19:32:26 -0300

    proyecto empleadas

---
+++ src/main/java/ar/com/ada/api/empleadas/controllers/EmpleadaController.java
@@ -0,0 +1,112 @@
+package ar.com.ada.api.empleadas.controllers;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import ar.com.ada.api.empleadas.entities.Categoria;
+import ar.com.ada.api.empleadas.entities.Empleada;
+import ar.com.ada.api.empleadas.entities.Empleada.EstadoEmpleadaEnum;
+import ar.com.ada.api.empleadas.models.request.InfoEmpleadaNueva;
+import ar.com.ada.api.empleadas.models.request.SueldoNuevoEmpleada;
+import ar.com.ada.api.empleadas.models.response.GenericResponse;
+import ar.com.ada.api.empleadas.services.CategoriaService;
+import ar.com.ada.api.empleadas.services.EmpleadaService;
+
+@RestController
+public class EmpleadaController {
+
+    @Autowired
+    private EmpleadaService service;
+
+    @Autowired
+    CategoriaService categoriaService;
+
+    @GetMapping("/empleados")
+    public ResponseEntity<List<Empleada>> traerEmpleadas() {
+        final List<Empleada> lista = service.traerEmpleadas();
+
+        return ResponseEntity.ok(lista);
+    }
+
+    @PostMapping("/empleados")
+    public ResponseEntity<?> crearEmpleada(@RequestBody  InfoEmpleadaNueva empleadaInfo) {
+        GenericResponse respuesta = new GenericResponse();
+
+        Empleada empleada = new Empleada();
+        empleada.setNombre(empleadaInfo.nombre);
+        empleada.setEdad(empleadaInfo.edad);
+        empleada.setSueldo(empleadaInfo.sueldo);
+        empleada.setFechaAlta(new Date());
+        
+        Categoria categoria = categoriaService.buscarCategoria(empleadaInfo.categoriaId);
+        empleada.setCategoria(categoria);
+        empleada.setEstado(EstadoEmpleadaEnum.ACTIVO);
+
+        service.crearEmpleada(empleada);
+        respuesta.isOk = true;
+        respuesta.id = empleada.getEmpleadaId();
+        respuesta.message = "La empleada fue creada con exito";
+        return ResponseEntity.ok(respuesta);
+
+    }
+
+    @GetMapping("/empleados/{id}")
+    public ResponseEntity<Empleada> getEmpleadaPorId(@PathVariable Integer id){
+        Empleada empleada = service.buscarEmpleada(id);
+
+        return ResponseEntity.ok(empleada);
+    }
+
+    //Detele/empleados/{id} --> Da de baja un empleado poniendo el campo estado en "baja"
+    // y la fecha de baja que sea el dia actual.
+    @DeleteMapping("/empleados/{id}")
+    public ResponseEntity<?> bajaEmpleada(@PathVariable Integer id){
+
+        service.bajaEmpleadaPorId(id);
+
+        GenericResponse respuesta = new GenericResponse();
+
+        respuesta.isOk = true;
+        respuesta.message = "La empleada fue dada de baja con exito";
+
+        return ResponseEntity.ok(respuesta);
+
+    }
+
+    //Get /empleados/categorias/{catId} --> Obtiene la lista de empleados de una categoria.
+    @GetMapping("/empleados/categorias/{catId}")
+    public ResponseEntity<List<Empleada>> obtenerEmpleadasPorCategoria(@PathVariable Integer catId){
+        
+        List<Empleada> empleadas = service.traerEmpleadaPorCategoria(catId);
+        return ResponseEntity.ok(empleadas);
+    }
+
+    @PutMapping("/empleados/{id}/sueldos")
+    public ResponseEntity<GenericResponse> modificarSueldo(@PathVariable Integer id, @RequestBody SueldoNuevoEmpleada sueldoNuevoInfo){
+
+        //1) buscar la empleada
+        Empleada empleada = service.buscarEmpleada(id);
+        //2) setear su nuevo sueldo
+        empleada.setSueldo(sueldoNuevoInfo.sueldoNuevo);
+        //3) guardarlo  en la base de datos
+        service.guardar(empleada);
+
+        GenericResponse respuesta = new GenericResponse();
+
+        respuesta.isOk = true;
+        respuesta.message = "Sueldo actualizado";
+
+        return ResponseEntity.ok(respuesta);
+    }
+}
---
+++ src/main/java/ar/com/ada/api/empleadas/services/EmpleadaService.java
@@ -0,0 +1,66 @@
+package ar.com.ada.api.empleadas.services;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.com.ada.api.empleadas.entities.Categoria;
+import ar.com.ada.api.empleadas.entities.Empleada;
+import ar.com.ada.api.empleadas.entities.Empleada.EstadoEmpleadaEnum;
+import ar.com.ada.api.empleadas.repos.CategoriaRepository;
+import ar.com.ada.api.empleadas.repos.EmpleadaRepository;
+
+@Service
+public class EmpleadaService {
+
+    @Autowired
+    EmpleadaRepository repo;
+
+    @Autowired
+    CategoriaService categoriaService;
+
+    public void crearEmpleada(Empleada empleada) {
+        repo.save(empleada);
+    }
+
+    public List<Empleada> traerEmpleadas() {
+        return repo.findAll();
+    }
+
+    public Empleada buscarEmpleada(Integer empleadaId){
+        Optional<Empleada> resultado = repo.findById(empleadaId);
+
+        if(resultado.isPresent())
+            return resultado.get();
+        
+        
+        return null;
+        
+    }
+
+    //DELETE LOGICO,que se mantiene en la db pero con estatus.
+	public void bajaEmpleadaPorId(Integer id) {
+        Empleada empleada = this.buscarEmpleada(id);
+        
+        empleada.setEstado(EstadoEmpleadaEnum.BAJA);
+        empleada.setFechaBaja(new Date());
+
+        repo.save(empleada);
+
+	}
+
+	public List<Empleada> traerEmpleadaPorCategoria(Integer catId) {
+		
+        Categoria categoria = categoriaService.buscarCategoria(catId);
+        
+        return categoria.getEmpleadas();
+
+	}
+
+	public void guardar(Empleada empleada) {
+        repo.save(empleada);
+	}
+}
